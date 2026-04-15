6ef717c1c6b47ca11b68520c14c4ba2fa6955cb9
markssants <marksbeys@proton.me>
2026-04-15 04:56:09 -0300

    Update executar.sh

--- executar.sh
+++ executar.sh
@@ -1,4 +1,4 @@
-cd /Users/marksbeys/Desktop/Marks/Scripts/baixar-curso
+cd /Users/marksbeys/Desktop/Marks/Scripts/baixar-curso # o seu diretorio de onde foi baixado o arquivo
 
 python3 -m venv venv
 source venv/bin/activate
